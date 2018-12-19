82c3cdb48d1ae96faaca0af9acde8d196261f5a0
rhoenkelevra <renerhoen@hotmail.com>
2018-12-19 12:48:35 +0900

    added calculate date function

--- app.js
+++ app.js
@@ -1,42 +1,50 @@
-// Setting Today's Date
-
+// #####Set Today's Date #####
 let today = new Date();
+// Format Today's date using .toLacaleDateString
 let options = {
-	weekday: 'long',
-	year: 'numeric',
-	month: 'long',
-	day: 'numeric'
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric"
 };
-let todayFormated = today.toLocaleDateString('en-US', options);
- document.getElementById('todayDate').textContent = todayFormated;
+let todayFormated = today.toLocaleDateString("en-US", options);
+document.getElementById("todayDate").textContent = todayFormated;
 
-/*var dd = today.getDate();
-let mm = today.getMonth() + 1;
-let yyyy = today.getFullYear();
-*/
+// ###### Setting Target Date ########
 
-// Setting Target Date
 // Getting innerHTML
-const inputedDate = parseInt(document.getElementById('dateInput').innerHTML);
-const inputedMonth = parseInt(document.getElementById('monthInput').innerHTML);
-const inputedYear = parseInt(document.getElementById('yearInput').innerHTML);
+const inputedDate = parseInt(document.getElementById("dateInput").innerHTML);
+const inputedMonth = parseInt(document.getElementById("monthInput").innerHTML);
+const inputedYear = parseInt(document.getElementById("yearInput").innerHTML);
 
+// Set the date Object from variable
 let futureDate = new Date();
-futureDate = futureDate.setDate(futureDate.getDate()+ inputedDate);
+futureDate.setDate(futureDate.getDate() + inputedDate);
 
 let futureMonth = new Date();
-futureMonth = futureMonth.setMonth(futureMonth.getMonth + inputedMonth);
+futureMonth.setMonth(futureMonth.getMonth() + inputedMonth);
+
+let futureYear = new Date();
+futureYear.setFullYear(futureYear.getFullYear() + inputedYear);
+
+// Put dates together and Format
+const dd = futureDate.getDate();
+const mm = futureMonth.getMonth() + 1;
+const yyyy = futureYear.getFullYear();
+let dateString = dd + "/" + mm + "/" + yyyy;
+
+// Display Result
+document.getElementById('dateResult').innerHTML = dateString;
+console.log(dateString);
 
 
+/*
+Next Step: 
+- get input from user
+- addEventListener on calculate button
+- try to get a callback function to format date
+- format like today's date
+- improve UI 
 
-let targetDate = new Date();
-const dd = targetDate.getDate();
-const mm = targetDate.getMonth() + 1;
-const yyyy = targetDate.getFullYear();
-let dateString = dd + '/' + mm + '/' + yyyy;
 
-console.log(dateString)
-//console.log(futureDateFormated);
-// trying to get a callback function to formate date 
-//function formatDate(date,)
-
+*/
